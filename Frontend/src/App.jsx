8f6ec0e9b09bb8056c87c744a8505cd241cd9f59
Matheus Medeiros Oselame <matheus.medeiros@nbwdigital.com.br>
2023-05-15 16:56:38 -0300

    Adicionando pastas e limpando projeto

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header" />
+    </div>
+  );
+}
+
+export default App;
